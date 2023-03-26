3117bcbc0f68c887a3f9b7956f9e6f0ca586a6b1
Oleksandr Horiainov <oleksander.horiainov@flickelectric.co.nz>
2023-03-26 20:14:42 +0300

    Add authentication with devise

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  devise :database_authenticatable
+  validates :username, presence: :true, uniqueness: true
+
+  scope :all_except, ->(user) { where.not(id: user.id) }
+end
